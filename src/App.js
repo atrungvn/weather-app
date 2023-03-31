a7a5b16ceed1f80857fb027d6859d667876a478a
atrungvn <trungnguyenbg@gmail.com>
2023-03-31 23:28:10 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,126 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import API_KEY from './axios';
+import axios from 'axios';
+
+let listIcon = [
+    {
+      type: "Clear",
+      img: "https://cdn-icons-png.flaticon.com/512/6974/6974833.png",
+    },
+    {
+      type: "Rain",
+      img: "https://cdn-icons-png.flaticon.com/512/3351/3351979.png",
+    },
+    {
+      type: "Snow",
+      img: "https://cdn-icons-png.flaticon.com/512/642/642102.png",
+    },
+    {
+      type: "Clouds",
+      img: "https://cdn-icons-png.flaticon.com/512/414/414825.png",
+    },
+    {
+      type: "Haze",
+      img: "https://cdn-icons-png.flaticon.com/512/1197/1197102.png",
+    },
+    {
+      type: "Smoke",
+      img: "https://cdn-icons-png.flaticon.com/512/4380/4380458.png",
+    },
+    {
+      type: "Mist",
+      img: "https://cdn-icons-png.flaticon.com/512/4005/4005901.png",
+    },
+    {
+      type: "Drizzle",
+      img: "https://cdn-icons-png.flaticon.com/512/3076/3076129.png",
+    },
+  ];
+
 
 function App() {
+  const [getState,setGetState] = useState();
+  const [state,setState] = useState();
+  const [temp,setTemp] = useState();
+  const [status,setStatus] = useState();
+  const [country,setCountry] = useState();
+  const [ok,setOk] = useState(false);
+  const [icon,setIcon] = useState();
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&units=metric&appid=${API_KEY}`;
+  
+
+  function handleInput(name) {
+    setGetState(name);
+  }
+
+  function handleSearch (e) {
+    e.preventDefault();
+    setState(getState);
+    console.log("search state: ", getState);
+  }
+
+  useEffect (() => {
+    console.log(apiUrl);
+    const getData = async () => {
+        await axios.get(apiUrl).then(res => {
+        setOk(true);
+        console.log(res);
+        const data = res.data;
+
+        console.log(data.name);
+        setState(data.name);
+        setTemp(data.main.temp);
+        setStatus(data.weather[0].main);
+        setCountry(data.sys.country);
+        console.log(data.weather[0].main);
+        
+       
+        
+        
+  
+      }).catch (err => {
+        setOk(false);
+        console.log(err)
+      });
+    }
+      getData();
+    
+  },[state])
+
+  function Show () {
+    let idx1 = (listIcon.findIndex((item) => item.type === status));
+    const imgUrl = listIcon[idx1].img;
+
+    return (
+    <div className='main'>
+      <div>{state}, {country}</div>
+      
+      <div ><img id="image" src={imgUrl} /></div>
+      <div className='status'>{status}</div>
+      <div className='temp'>{temp}&deg;C</div>
+    </div>
+    )
+  }
+
+  function ShowErr () {
+
+    return <div>Not Found</div>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSearch}>
+          <input type="text" id="input"
+          onChange={ (e) => {handleInput(e.target.value)}} 
+          placeholder="Enter to search"></input>
+          <button type="submit">Search</button>
+        </form>
+        
+        <div>{ok? <Show /> : <ShowErr />}</div>
       </header>
     </div>
   );
---
+++ src/axios/index.js
@@ -0,0 +1,3 @@
+const API_KEY = '4126b326e7bec62b692207120c6c41ce';
+
+export default API_KEY;
